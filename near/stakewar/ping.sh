4882cb2626eb7ca08fc578837dd2b2edeab06e85
[NODERS]TEAM <94483941+nodersteam@users.noreply.github.com>
2022-08-10 16:58:19 +0300

    Update ping.sh

--- near/stakewar/ping.sh
+++ near/stakewar/ping.sh
@@ -3,6 +3,6 @@ export NEAR_ENV=shardnet
 echo 'export NEAR_ENV=shardnet' >> ~/.bash_profile
 source ~/.bash_profile
 near call <NAME>.factory.shardnet.near ping '{}' --accountId <NAME>.shardnet.near --gas=300000000000000 >> /root/logs/all.log
-near proposals | grep noders >> /root/logs/all.log
-near validators current | grep noders >> /root/logs/all.log
-near validators next | grep noders >> /root/logs/all.log
+near proposals | grep <NAME> >> /root/logs/all.log
+near validators current | grep <NAME> >> /root/logs/all.log
+near validators next | grep <NAME> >> /root/logs/all.log
