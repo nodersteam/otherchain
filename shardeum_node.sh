2a2586b5545c101720a468ec33bb2e985f583d20
[NODERS]TEAM <94483941+nodersteam@users.noreply.github.com>
2023-09-04 13:48:50 +0700

    Update shardeum_node.sh

--- shardeum_node.sh
+++ shardeum_node.sh
@@ -18,6 +18,7 @@ PS3='Select an action: '
 options=(
 "Prepare the server for installation"
 "Install Shardeum Node"
+"Update Node"
 "Dashboard addres"
 "Stop and delite node"
 "Exit")
@@ -420,6 +421,45 @@ To use the Command Line Interface:
  3. Run "operator-cli --help" for commands
 
 EOF
+break
+;;
+
+"Update Node")
+
+command -v docker >/dev/null 2>&1 || { echo >&2 "'docker' is required but not installed. See https://gitlab.com/shardeum/validator/dashboard/-/tree/dashboard-gui-nextjs#how-to for details."; exit 1; }
+if command -v docker-compose &>/dev/null; then
+  echo "docker-compose is installed on this machine"
+elif docker --help | grep -q "compose"; then
+  echo "docker compose subcommand is installed on this machine"
+else
+  echo "docker-compose or docker compose is not installed on this machine"
+  exit 1
+fi
+
+docker-safe() {
+  if ! command -v docker &>/dev/null; then
+    echo "docker is not installed on this machine"
+    exit 1
+  fi
+
+  if ! docker $@; then
+    echo "Trying again with sudo..."
+    sudo docker $@
+  fi
+}
+
+echo "Updating validator image and rebuilding docker..."
+./docker-down.sh
+echo "Clearing validator images..."
+./cleanup.sh
+echo "Updating local repo..."
+git pull origin main
+echo "Rebuilding local validator image..."
+docker-safe build --no-cache -t local-dashboard -f Dockerfile --build-arg RUNDASHBOARD=y .
+./docker-up.sh
+echo "Starting image. This could take a while..."
+(docker-safe logs -f shardeum-dashboard &) | grep -q 'done'
+
 break
 ;;
 
